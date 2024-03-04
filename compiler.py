e500da6443b5a151878eb270127de983304ce202
PanagiotisT2307 <trypos2307@gmail.com>
2024-03-04 21:28:40 +0200

    Creating the first states

--- compiler.py
+++ compiler.py
@@ -1,5 +1,31 @@
 ### Functions
+
 def lexicalAnalyzer():
-  return null
+	state = 0
+	while (state != -1 or state != -2):
+		## Check for ID
+		if (state == 0 and input in letters):
+			state = 1
+		elif (state == 1 and input in letters or input in digits):
+			state = 1
+		
+		## Check for Number
+		elif (state == 0 and input in digits):
+			state = 2
+		elif (state == 2 and input in digits):
+			state = 2
+			
+		## Check for Operator
+		elif (state == 0 and input in operator):
+			state = -2
+		
+		## Check for symbol #
+		elif (state == 0 and input == "#"):
+			state = 3
+		elif (state == 3 and input == "#"):
+			state = 4
+			
+			
+	  
 
 ### Main Program
