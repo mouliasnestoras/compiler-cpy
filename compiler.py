f1e487db2f260290982ad57d864de334273b4f19
PanagiotisT2307 <trypos2307@gmail.com>
2024-03-05 14:21:58 +0200

    Basic code

--- compiler.py
+++ compiler.py
@@ -1,1 +1,26 @@
+import sys
 
+### Functions
+def lexicalAnalyzer():
+    global code_line
+    global code_index
+    
+    return (0, 0, 0)
+
+
+def startProcessing():
+    lexicalAnalyzer()
+    
+### Main Program
+
+if __name__ == "__main__":
+    code_line = 0
+    code_index = 0
+    
+    keywords = ("main", "def", "#def", "#int", "global", "if",
+                "elif", "else", "while", "print", "return",
+                "input", "int", "and", "or", "not")
+
+    code_file = open(sys.argv[1], "r")
+    startProcessing()
+    
