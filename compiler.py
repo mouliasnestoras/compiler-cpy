d6ed2b77cd81edd0089699ed21718b3b2da74844
PanagiotisT2307 <trypos2307@gmail.com>
2024-03-05 15:10:28 +0200

    Start creating the lexical analyzer

--- compiler.py
+++ compiler.py
@@ -11,16 +11,4 @@ def lexicalAnalyzer():
 def startProcessing():
     lexicalAnalyzer()
     
-### Main Program
-
-if __name__ == "__main__":
-    code_line = 0
-    code_index = 0
-    
-    keywords = ("main", "def", "#def", "#int", "global", "if",
-                "elif", "else", "while", "print", "return",
-                "input", "int", "and", "or", "not")
-
-    code_file = open(sys.argv[1], "r")
-    startProcessing()
     
